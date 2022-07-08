31d53fd5b533268c3ed0dddc9ef4d23cd8c44843
Kevin Pitti <kepc08@gmail.com>
2022-07-08 11:30:10 -0600

    gtag added

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,22 @@ module.exports = {
     siteUrl: `https://kevinpc.com`,
     description: `Kevin es un un desarrollador web enfocado en trabajar principalmente en el frontend pero también el backend de aplicaciones web. Amante de la tecnología.`
   },
-  plugins: ["gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-9TLRW87B6J"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-6DBZ5LZYRZ", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
       resolve: 'gatsby-plugin-manifest',
       options: {
         "icon": "src/images/logo.png"
