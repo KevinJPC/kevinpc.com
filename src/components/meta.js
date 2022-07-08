b2a13bfc4abe49d0df0d4a295d7dcd2f3f76ad40
Kevin Pitti <kepc08@gmail.com>
2022-07-08 16:19:10 -0600

    meta added

--- src/components/meta.js
+++ src/components/meta.js
@@ -10,7 +10,6 @@ const Meta = () => <Helmet htmlAttributes={{ lang: 'es' }}>
     <link rel="canonical" href="https://kevinpc.com" />
     <meta property="og:title" content="Kevin Pitti Castro | Desarrollador web" />
     <meta property="og:image" content="https://kevinpc.com/og.png" />
-    <meta property="og:description" content="Kevin es un un desarrollador web enfocado en trabajar principalmente en el frontend pero también el backend de aplicaciones web. Amante de la tecnología." />
     <meta property="og:type" content="website" />
     <meta property='og:url' content="https://kevinpc.com" />
 </Helmet>
